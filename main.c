3063a579531fdd318b99ed558e7cc46d86162663
Digit16 <tomasz.wrobel.dev@gmail.com>
2023-11-28 18:14:53 +0100

    fix indentation

--- main.c
+++ main.c
@@ -5,28 +5,28 @@
 
 
 #define MAX(a,b) \
-   ({ __typeof__ (a) _a = (a); \
-      __typeof__ (b) _b = (b); \
-      _a > _b ? _a : _b; })
+    ({ __typeof__ (a) _a = (a); \
+        __typeof__ (b) _b = (b); \
+        _a > _b ? _a : _b; })
 
 
 enum Tag {
-   REQ_HOTEL,
-   REQ_GUIDE,
-   ACK_HOTEL,
-   ACK_GUIDE,
-   RELEASE_HOTEL,
-   RELEASE_GUIDE,
-   FINISHED,
+    REQ_HOTEL,
+    REQ_GUIDE,
+    ACK_HOTEL,
+    ACK_GUIDE,
+    RELEASE_HOTEL,
+    RELEASE_GUIDE,
+    FINISHED,
 };
 
 
 enum State {
-   REST,                // Waiting
-   WAIT_HOTEL,          // Requesting Hotel
-   INSECTION_HOTEL,     // In section Hotel
-   WAIT_GUIDE,          // Requesting Guide
-   INSECTION_GUIDE,     // In section Hotel and in section Guide
+    REST,                // Waiting
+    WAIT_HOTEL,          // Requesting Hotel
+    INSECTION_HOTEL,     // In section Hotel
+    WAIT_GUIDE,          // Requesting Guide
+    INSECTION_GUIDE,     // In section Hotel and in section Guide
 };
 
 
@@ -55,69 +55,69 @@ pthread_mutex_t clock_guard = PTHREAD_MUTEX_INITIALIZER;
 
 // send packet scalar and vector clocks
 void send_packet(int dest, enum Tag tag) {
-   pthread_mutex_lock(&clock_guard);
-   ++scalar_ts;
-   ++vector_ts[rank];
-   int position = 0;
-   MPI_Pack(&scalar_ts, 1, MPI_INT, send_buffer, buffer_size, &position, MPI_COMM_WORLD);
-   MPI_Pack(vector_ts, size, MPI_INT, send_buffer, buffer_size, &position, MPI_COMM_WORLD);
-   MPI_Send(send_buffer, position, MPI_PACKED, dest, (int)tag, MPI_COMM_WORLD);
-   pthread_mutex_unlock(&clock_guard);
+    pthread_mutex_lock(&clock_guard);
+    ++scalar_ts;
+    ++vector_ts[rank];
+    int position = 0;
+    MPI_Pack(&scalar_ts, 1, MPI_INT, send_buffer, buffer_size, &position, MPI_COMM_WORLD);
+    MPI_Pack(vector_ts, size, MPI_INT, send_buffer, buffer_size, &position, MPI_COMM_WORLD);
+    MPI_Send(send_buffer, position, MPI_PACKED, dest, (int)tag, MPI_COMM_WORLD);
+    pthread_mutex_unlock(&clock_guard);
 }
 
 // recieve packet with scalar and vector clocks
 void recv_packet(MPI_Status* status) {
-   MPI_Recv(recv_buffer, buffer_size, MPI_PACKED, MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, status);
-   pthread_mutex_lock(&clock_guard);
-   int position = 0;
-   int ts;
-   MPI_Unpack(recv_buffer, buffer_size, &position, &ts, 1, MPI_INT, MPI_COMM_WORLD);
-   scalar_ts = MAX(scalar_ts, ts) + 1;
-   last_received_scalar_ts[status->MPI_SOURCE] = scalar_ts;
-   for (int i = 0; i < size; ++i) {
-      MPI_Unpack(recv_buffer, buffer_size, &position, &ts, 1, MPI_INT, MPI_COMM_WORLD);
-      vector_ts[i] = MAX(vector_ts[i], ts) + 1;
-   }
-   pthread_mutex_unlock(&clock_guard);
+    MPI_Recv(recv_buffer, buffer_size, MPI_PACKED, MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, status);
+    pthread_mutex_lock(&clock_guard);
+    int position = 0;
+    int ts;
+    MPI_Unpack(recv_buffer, buffer_size, &position, &ts, 1, MPI_INT, MPI_COMM_WORLD);
+    scalar_ts = MAX(scalar_ts, ts) + 1;
+    last_received_scalar_ts[status->MPI_SOURCE] = scalar_ts;
+    for (int i = 0; i < size; ++i) {
+        MPI_Unpack(recv_buffer, buffer_size, &position, &ts, 1, MPI_INT, MPI_COMM_WORLD);
+        vector_ts[i] = MAX(vector_ts[i], ts) + 1;
+    }
+    pthread_mutex_unlock(&clock_guard);
 }
 
 
 void* listener_loop(void* arg) {
-   MPI_Status status;
+    MPI_Status status;
 
-   int finished_counter = 0;
+    int finished_counter = 0;
 
-   while(finished_counter < size) {
-      recv_packet(&status);
+    while(finished_counter < size) {
+        recv_packet(&status);
 
-      switch (status.MPI_TAG) {
-         case REQ_HOTEL: {
+        switch (status.MPI_TAG) {
+            case REQ_HOTEL: {
             // TODO: add request to list
             send_packet(status.MPI_SOURCE, ACK_HOTEL);
-         } break;
-         case REQ_GUIDE: {
+            } break;
+            case REQ_GUIDE: {
             // TODO: add request to list
             send_packet(status.MPI_SOURCE, ACK_GUIDE);
-         } break;
-         case ACK_HOTEL: {
+            } break;
+            case ACK_HOTEL: {
             ++ack_hotel_counter;
-         } break;
-         case ACK_GUIDE: {
+            } break;
+            case ACK_GUIDE: {
             ++ack_guide_counter;
-         } break;
-         case RELEASE_HOTEL: {
+            } break;
+            case RELEASE_HOTEL: {
             // TODO: remove from requests list
-         } break;
-         case RELEASE_GUIDE: {
+            } break;
+            case RELEASE_GUIDE: {
             // TODO: remove from requests list
-         } break;
-         case FINISHED: {
+            } break;
+            case FINISHED: {
             ++finished_counter;
-         } break;
+            } break;
 
-         // TODO: check ack counter conditions
-      }
-   }
+            // TODO: check ack counter conditions
+        }
+    }
 }
 
 
