09b805da683148d150f7437daa980c6d50a6c43f
Digit16 <tomasz.wrobel.dev@gmail.com>
2023-11-28 18:02:25 +0100

    fix MAX macro (prevent double evaluation)

--- main.c
+++ main.c
@@ -4,7 +4,11 @@
 #include <pthread.h>
 
 
-#define MAX(a, b) ((a) > (b) ? (a) : (b))
+#define MAX(a,b) \
+   ({ __typeof__ (a) _a = (a); \
+      __typeof__ (b) _b = (b); \
+      _a > _b ? _a : _b; })
+
 
 enum Tag {
    REQ_HOTEL,
