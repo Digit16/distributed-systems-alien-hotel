f47efa3e770a8d011f47ac2a093f3583a2d943fa
Digit16 <tomasz.wrobel.dev@gmail.com>
2023-11-28 19:39:23 +0100

    add threads

--- main.c
+++ main.c
@@ -29,8 +29,15 @@ typedef enum State {
     INSECTION_GUIDE,     // In section Hotel and in section Guide
 } State;
 
+typedef enum ProcessType {
+    ALIEN_PURPLE,
+    ALIEN_BLUE,
+    CLEANER,
+} ProcessType;
+
 // current state of the process
 State state = REST;
+ProcessType process_type;
 
 // process id and number of processes
 int rank;
@@ -74,8 +81,10 @@ void send_packet(int dest, Tag tag) {
 
 
 // recieve packet with scalar and vector clocks
-void recv_packet(MPI_Status* status) {
-    MPI_Recv(recv_buffer, buffer_size, MPI_PACKED, MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, status);
+MPI_Status recv_packet(void) {
+    MPI_Status status;
+
+    MPI_Recv(recv_buffer, buffer_size, MPI_PACKED, MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
     pthread_mutex_lock(&clock_guard);
 
     // TODO: add debug message
@@ -84,7 +93,7 @@ void recv_packet(MPI_Status* status) {
     int ts;
     MPI_Unpack(recv_buffer, buffer_size, &position, &ts, 1, MPI_INT, MPI_COMM_WORLD);
     scalar_ts = MAX(scalar_ts, ts) + 1;
-    last_received_scalar_ts[status->MPI_SOURCE] = scalar_ts;
+    last_received_scalar_ts[status.MPI_SOURCE] = scalar_ts;
 
     ++vector_ts[rank];
     for (int i = 0; i < size; ++i) {
@@ -92,6 +101,8 @@ void recv_packet(MPI_Status* status) {
         vector_ts[i] = MAX(vector_ts[i], ts);
     }
     pthread_mutex_unlock(&clock_guard);
+    
+    return status;
 }
 
 
@@ -106,12 +117,10 @@ void change_state(State new_state) {
 
 
 void* listener_loop(void* arg) {
-    MPI_Status status;
-
     int finished_counter = 0;
 
     while(finished_counter < size) {
-        recv_packet(&status);
+        MPI_Status status = recv_packet();
 
         switch (status.MPI_TAG) {
             case REQ_HOTEL: {
@@ -161,28 +170,40 @@ void* cleaner_loop(void* arg) {
 
 int main(int argc, char** argv) {
 
-   MPI_Init(&argc, &argv);
-   MPI_Comm_size(MPI_COMM_WORLD, &size);
-   MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Init(&argc, &argv);
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 
-   vector_ts = calloc(size * sizeof(int), 0);
-   last_received_scalar_ts = calloc(size * sizeof(int), 0);
+    vector_ts = calloc(size * sizeof(int), 0);
+    last_received_scalar_ts = calloc(size * sizeof(int), 0);
 
-   buffer_size = (1 + size) * sizeof(int);
-   send_buffer = malloc(buffer_size);
-   recv_buffer = malloc(buffer_size);
+    buffer_size = (1 + size) * sizeof(int);
+    send_buffer = malloc(buffer_size);
+    recv_buffer = malloc(buffer_size);
 
-   
-   send_packet((rank + 1) % size, ACK_HOTEL);
-   MPI_Status status;
-   recv_packet(&status);
-   printf("%d - received from %d\n", rank, status.MPI_SOURCE);
+    // TODO: create a function to determine process type
+    process_type = ALIEN_BLUE;
+
+    pthread_t main_thread, listener_thread;
 
 
-   MPI_Finalize();
-   free(vector_ts);
-   free(send_buffer);
-   free(recv_buffer);
+    if (process_type == ALIEN_BLUE || process_type == ALIEN_PURPLE) {
+        pthread_create(&main_thread, (void*)0, alien_loop, (void*)0);
+    } else {
+        pthread_create(&main_thread, (void*)0, cleaner_loop, (void*)0);
+    }
 
-   return EXIT_SUCCESS;
+    pthread_create(&listener_thread, (void*)0, listener_loop, (void*)0);
+
+
+    pthread_join(main_thread, (void*)0);
+    pthread_join(listener_thread, (void*)0);
+
+
+    MPI_Finalize();
+    free(vector_ts);
+    free(send_buffer);
+    free(recv_buffer);
+
+    return EXIT_SUCCESS;
 }
